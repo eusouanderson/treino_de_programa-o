6d0fb4614134f5bad9da604d9b767e5a4dfee598
Anderson Rodrigues <eusouanderson@outlook.com>
2025-08-20 23:56:26 -0300

    refactor: cleanup project structure and update API routes and services

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <AppLogin />
+  <h1>Aqui é A Raiz</h1>
+</template>
+<script>
+import AppLogin from './components/AppLogin.vue'
+
+export default {
+  components: { AppLogin },
+  setup() {},
+}
+</script>
+<style scoped></style>
+
+<style>
+h1 {
+  background-color: aqua;
+}
+</style>
---
+++ src/components/AppLogin.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Login</h1>
+  <p>Silas aqui que vai acontecer a arte !!</p>
+</template>
+
+<script></script>
+
+<style></style>
